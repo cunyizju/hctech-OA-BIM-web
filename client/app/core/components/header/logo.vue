22f984f23af30b63c9c90e09fae0385bf9c83b6b
LCY <cunyizju@126.com>
2019-11-14 14:17:54 +0800

    first commit

---
+++ client/app/core/components/header/logo.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+	.logo
+		a.nav-item(href="#")
+			span 
+				strong 恒城
+				| 科技
+</template>
+
+<script>
+
+	export default {
+
+	};
+	
+</script>
+
+<style lang="scss">
+</style>
