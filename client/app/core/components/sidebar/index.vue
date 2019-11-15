552a448985890b01dd7709cca0ee3697dea40117
LCY <cunyizju@126.com>
2019-11-15 14:15:55 +0800

    skeleton

--- client/app/core/components/sidebar/index.vue
+++ client/app/core/components/sidebar/index.vue
@@ -3,17 +3,48 @@
 		.menu
 			.title {{ "General" | i18n }}
 			ul
-				router-link(tag="li", to="/")
-					a(:title="_('Home')")
+				router-link(tag="li", to="/design")
+					a(:title="_('设计服务')")
 						span.icon
-							i.fa.fa-home
-						span.label {{ "Home" | i18n }}
+							i.fa.fa-bank
+						span.label {{ "设计服务" | i18n }} 
+					
+						router-link(tag="li", to="/design/general")
+							a(:title="_('基本信息')")
+								span.icon
+									i.fa.fa-anchor
+								span.label {{ "基本信息" | i18n }}
+				
 
-				router-link(tag="li", to="/")
-					a(:title="_('Allelement')")
-						span.icon
-							i.fa.fa-home
-						span.label {{ "Allelement" | i18n }}
+						router-link(tag="li", to="/design/frontpage")
+							a(:title="_('项目首页')")
+								span.icon
+									i.fa.fa-bookmark
+								span.label {{ "项目首页" | i18n }}
+
+						router-link(tag="li", to="/design/progress")
+							a(:title="_('进程概览')")
+								span.icon
+									i.fa.fa-calendar
+								span.label {{ "进程概览" | i18n }}
+
+						router-link(tag="li", to="/design/filesmanager")
+							a(:title="_('资料管理')")
+								span.icon
+									i.fa.fa-archive
+								span.label {{ "资料管理" | i18n }}
+
+						router-link(tag="li", to="/design/dwgcompare")
+							a(:title="_('图纸对比')")
+								span.icon
+									i.fa.fa-newspaper-o
+								span.label {{ "图纸对比" | i18n }}
+
+				//- router-link(tag="li", to="/")
+				//- 	a(:title="_('Allelement')")
+				//- 		span.icon
+				//- 			i.fa.fa-home
+				//- 		span.label {{ "Allelement" | i18n }}
 
 				router-link(tag="li", to="/counter")
 					a(:title="_('Demo')")
