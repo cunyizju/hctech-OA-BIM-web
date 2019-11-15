6b5c99b86863a7c5d4e22693688a68a4d8c28fb5
LCY <cunyizju@126.com>
2019-11-15 15:15:03 +0800

    blankpages-skeleton

--- client/app/core/router.js
+++ client/app/core/router.js
@@ -6,6 +6,10 @@ import VueRouter from "vue-router";
 import Home from "../modules/home";
 //import Design from "../modules/design";
 import General from "../modules/design/general";
+import Frontpage from "../modules/design/frontpage";
+import Filemanager from "../modules/design/filemanager";
+import Progress from "../modules/design/progress";
+import Dwgcompare from "../modules/design/dwgcompare";
 import Allelement from "../modules/all-element";
 import Counter from "../modules/counter";
 import Devices from "../modules/devices";
@@ -20,6 +24,10 @@ export default new VueRouter({
 		{ path: "/", component: Home },
 		//{ path: "/design", component: Design },
 		{ path: "/design/general", component: General },
+		{ path: "/design/frontpage", component: Frontpage },
+		{ path: "/design/progress", component: Progress },
+		{ path: "/design/filemanager", component: Filemanager },
+		{ path: "/design/dwgcompare", component: Dwgcompare },
 		//{ path: "/allelement", component: Allelement },
 		{ path: "/devices", component: Devices },
 		{ path: "/posts", component: Posts },
